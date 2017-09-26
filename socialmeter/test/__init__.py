114760c1e3c422da5978bc4e0684fc188adb8d1b
thomas-ring <thomasjring@icloud.com>
2017-09-25 20:40:38 -0400

    Add test suite module and kfold file but no implementation

---
+++ socialmeter/test/__init__.py
@@ -0,0 +1,9 @@
+# test.__init__.py
+#
+# This test suite is used to test uses of the chain and
+# configuration accuracy.
+#
+# Note: This is not a suite of unit tests for the accuracy
+# of the code, that is found in the root directory in 'tests'.
+
+from . import kfold
