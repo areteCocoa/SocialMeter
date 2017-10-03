4e602eb25007551da6b2404c90b7abd255134e4e
thomas-ring <thomasjring@icloud.com>
2017-10-02 20:55:16 -0400

    Change ExcessiveCapitalsFE to do a percentage instead of a count

--- socialmeter/preclass/excesscaps.py
+++ socialmeter/preclass/excesscaps.py
@@ -1,12 +1,18 @@
 # excesscaps.py
 
+import nltk
 from ..chain_links import FeatureExtractor
 
 
 class ExcessiveCapitalsFE(FeatureExtractor):
     def extract(self, text):
-        caps = 0
-        for ch in text:
-            if ch.lower() != ch:
-                caps += 1
-        return self.discretize_result(caps)
+        all_caps = 0
+        tokens = nltk.word_tokenize(text)
+        for t in tokens:
+            if t.upper() == t:
+                all_caps += 1
+        t = len(tokens)
+        if t == 0:
+            return 0
+        perc = all_caps / t
+        return self.discretize_result(perc)
